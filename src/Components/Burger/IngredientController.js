2cb225cd18d3ed96cee1f22b4ddc395c4a50f809
LAPTOP-VFBLUQ2U\USER <matthewfelix88@gmail.com>
2022-06-27 17:01:39 +0700

    test

--- src/Components/Burger/IngredientController.js
+++ src/Components/Burger/IngredientController.js
@@ -7,8 +7,8 @@ const IngredientController = () => {
     const dispatch=useDispatch()
   return (
     <div>
-        <button onClick={ ()=> dispatch(burgerSlice.actions.addIngredient({ingredient:'patty'}) )}>Add Patty</button>
-        <button onClick={ ()=> dispatch(burgerSlice.actions.addIngredient({ingredient:'lettuce'}) )}>Add Lettuce</button>
+        <button onClick={ ()=> dispatch(burgerSlice.actions.addIngredient({ingredient:'patty'}) )}>Add Patty </button>
+        <button onClick={ ()=> dispatch(burgerSlice.actions.addIngredient({ingredient:'lettuce'}) )}>Add Lettuce </button>
         <button onClick={ ()=> dispatch(burgerSlice.actions.addIngredient({ingredient:'cheese'}) )}>Add Cheese</button>
         <button onClick={ ()=> dispatch(burgerSlice.actions.addIngredient({ingredient:'tomato'}) )}>Add Tomato</button>
     </div>
